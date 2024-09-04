a236dd13810ec59ca6e64aa93dc8e0f5cd68a997
akafortune <hcm092002@gmail.com>
2024-09-03 20:32:16 -0400

    nk cell paths

---
+++ Assets/Scripts/NK_Pathing.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NK_Pathing : MonoBehaviour
+{
+    public enum NK_State
+    {
+        WAITING,
+        WALKING,
+        TURNING,
+        STUNNED,
+        REACHED_TARGET,
+        DEAD
+    }
+
+    public Transform[] walkingTargets;
+    public int currTarget = 0;
+    public float speed;
+    public NK_State currState;
+
+    public float turnClock = 0, turnTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currState = NK_State.WAITING;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Walk();
+    }
+
+    public void Walk()
+    {
+        if(currState == NK_State.WALKING)
+        {
+            this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, walkingTargets[currTarget].position, speed * Time.deltaTime);
+
+            if (this.gameObject.transform.position == walkingTargets[currTarget].position)
+            {
+                if(currTarget + 1 >= walkingTargets.Length)
+                {
+                    currTarget++;
+                    currState = NK_State.REACHED_TARGET;
+                } else
+                {
+                    currState = NK_State.TURNING;
+                }
+            }
+        }
+
+        if(currState == NK_State.TURNING)
+        {
+            turnClock += Time.deltaTime;
+
+            if(turnClock >= turnTimer)
+            {
+                turnClock = 0;
+                currTarget++;
+                currState = NK_State.WALKING;
+            }
+        }
+    }
+}
