9d639af391e6bf18609ca7e7fd651167383ddff5
akafortune <hcm092002@gmail.com>
2024-11-07 18:35:44 -0500

    basic note system

---
+++ Assets/Scripts/Note_Text_Display.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Note_Text_Display : MonoBehaviour
+{
+    public string noteTitle, noteBody;
+    public TextMeshProUGUI noteTitleDisplay, noteBodyDisplay;
+    public GameObject noteCanvas;
+
+    private bool noteDisplayed = false, readingNote = false;
+    // Start is called before the first frame update
+    
+    void Update()
+    {
+        if (noteDisplayed)
+        {
+            NoteInit();
+        }
+
+        if (readingNote)
+        {
+            if (Input.GetButtonDown("Stun"))
+            {
+                NoteDestroy();
+            }
+        }
+    }
+
+    void NoteDestroy()
+    {
+        Player_Movement.playerState = Player_Movement.MovementStates.STANDING;
+        noteCanvas.SetActive(false);
+        readingNote = false;
+        this.gameObject.SetActive(false);
+    }
+
+    void NoteInit()
+    {
+        noteTitleDisplay.text = noteTitle;
+        noteBodyDisplay.text = noteBody;
+        Player_Movement.playerState = Player_Movement.MovementStates.ACTING;
+        noteCanvas.SetActive(true);
+        readingNote = true;
+        noteDisplayed = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        noteDisplayed = true;
+    }
+}
