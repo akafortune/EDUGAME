e4bca17e2b9a112f9755dcf6a1254e8f5edd3ef9
ShlamStudiosHt <arcoblenohitman@gmail.com>
2024-11-17 23:39:07 -0500

    put notes down, and animaaaations

--- Assets/Scripts/Bullet Enemy/Bullet_Enemy.cs
+++ Assets/Scripts/Bullet Enemy/Bullet_Enemy.cs
@@ -7,13 +7,14 @@ public class Bullet_Enemy : MonoBehaviour
     public bool inRange = false, stunned = true;
     public GameObject bullet, enemyInRange = null;
     public Transform spawnPos;
+    public Animator anim;
 
     public float fireTime, stunTime;
     private float fireTimer = 0, stunTimer = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -61,6 +62,10 @@ public class Bullet_Enemy : MonoBehaviour
 
         if (fireTimer >= fireTime)
         {
+            //need to calculate direction of player from the enemy to determine which direction to use for animation
+            //triggers attack animation
+            anim.SetTrigger("Attack Trigger");
+            anim.SetFloat("moveX", 1);
             Instantiate(bullet, spawnPos.position, this.transform.rotation);
             fireTimer = 0;
         }
